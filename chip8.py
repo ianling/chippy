8d3c3825bf68d95d3015bb804b20710661704035
ianl <ianl@gofreewire.com>
2015-12-23 16:23:59 -0800

    inital commit!

---
+++ chip8.py
@@ -0,0 +1,9 @@
+class Chip8():
+    def __init__(self):
+        #initialize graphics, audio
+        #see https://github.com/metulburr/pong/blob/master/data/control.py
+        #graphics = 64x32
+        pass
+
+    def run(self):
+        pass
---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+main
+
+
+"""
+from .chip8 import Chip8
+
+def main():
+    app = Chip8()
+    app.run()
