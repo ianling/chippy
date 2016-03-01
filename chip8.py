d814c0ea27e5da0ea42d855336021d1c1b1eb7da
ianl <ianl@gofreewire.com>
2016-03-01 11:55:42 -0800

    Finished most opcodes

--- chip8.py
+++ chip8.py
@@ -1,6 +1,6 @@
 import array
 import binascii
-
+from random import randint
 
 class Chip8:
     def __init__(self, name):
@@ -133,18 +133,44 @@ class Chip8:
             self.v[int(opcode[1], 16)] = hex(intVX - intVY)[2:]
 
         # 8XY6 - Shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift.
+        elif opcode[0] == "8" and opcode[3] == "6":
+            intVX = int(self.v[int(opcode[1], 16)], 16)
+            binVX = bin(intVX)
+            self.v[int(opcode[1], 16)] = intVX >> 1
+            self.v[0xf] = binVX[-1]
 
 
         # 8XY7 - Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
-
+        elif opcode[0] == "8" and opcode[3] == "7":
+            intVX = int(self.v[int(opcode[1], 16)], 16)
+            intVY = int(self.v[int(opcode[2], 16)], 16)
+            if intVY - intVX < 0:
+                self.v[0xf] = 0
+            else:
+                self.v[0xf] = 1
+            self.v[int(opcode[1], 16)] = hex(intVY - intVX)[2:]
 
         # 8XYE - Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift
+        elif opcode[0] == "8" and opcode[3] == "E":
+            intVX = int(self.v[int(opcode[1], 16)], 16)
+            binVX = format(intVX, '#010b')
+            self.v[int(opcode[1], 16)] = intVX << 1
+            self.v[0xf] = binVX[2]
 
+        # 9XY0 - Skips the next instruction if VX doesn't equal VY.
+        elif opcodeFirstByte[0] == "9":
+            if self.v[int(opcode[1], 16)] != self.v[int(opcode[2], 16)]:
+                pcIncrementBy = 4 # jump ahead 4 bytes instead of the usual 2
 
 
         # ANNN - Sets I to the address NNN
         elif opcodeFirstByte[0] == "A":
             self.i = opcode[1:]
+
+        # CXNN - Sets V[X] to the result of a bitwise and operation on a random number and NN.
+        elif opcodeFirstByte[0] == "C":
+            self.v[int(opcode[1], 16)] = hex(int(opcode[2:], 16) & randint(0,255))[2:]
+
 
         # DXYN - Sprites stored in memory at location in index register (I), 8bits wide.
         #        Wraps around the screen. If when drawn, it clears a pixel,
@@ -155,6 +181,65 @@ class Chip8:
         #        If N is greater than 1, second line continues at position V[X], V[Y+1], and so on.
         elif opcodeFirstByte[0] == "D":
             pass
+
+        # EX9E - Skips the next instruction if the key stored in VX is pressed.
+        elif opcodeFirstByte[0] == "E" and opcodeLastByte == "9E":
+            pass
+
+        # EXA1 - Skips the next instruction if the key stored in VX isn't pressed.
+        elif opcodeFirstByte[0] == "E" and opcodeLastByte == "A1":
+            pass
+
+        # FX07 - Sets VX to the value of the delay timer.
+        elif opcodeFirstByte[0] == "F" and opcodeLastByte == "07":
+            self.v[int(opcode[1], 16)] = self.delayTimer
+
+        # FX0A - A key press is awaited, and then stored in VX.
+        elif opcodeFirstByte[0] == "F" and opcodeLastByte == "0A":
+            pass
+
+        # FX15 - Sets the delay timer to VX.
+        elif opcodeFirstByte[0] == "F" and opcodeLastByte == "15":
+            self.delayTimer = self.v[int(opcode[1], 16)]
+
+        # FX18 - Sets the sound timer to VX.
+        elif opcodeFirstByte[0] == "F" and opcodeLastByte == "18":
+            self.soundTimer = self.v[int(opcode[1], 16)]
+
+        # FX1E- Adds VX to I.
+        elif opcodeFirstByte[0] == "F" and opcodeLastByte == "1E":
+            intVX = int(self.v[int(opcode[1], 16)], 16)
+            intI = int(self.i, 16)
+            self.i = hex(intI + intVX)[2:]
+
+        # FX29- Sets I to the location of the sprite for the character in VX.
+        #       Characters 0-F (in hexadecimal) are represented by a 4x5 font.
+        elif opcodeFirstByte[0] == "F" and opcodeLastByte == "29":
+            pass
+
+        # FX33- Stores the Binary-coded decimal representation of VX,
+        #       with the most significant of three digits at the address in I,
+        #       the middle digit at I plus 1, and the least significant digit at I plus 2.
+        #       (In other words, take the decimal representation of VX,
+        #       place the hundreds digit in memory at location in I,
+        #       the tens digit at location I+1, and the ones digit at location I+2.)
+        elif opcodeFirstByte[0] == "F" and opcodeLastByte == "33":
+            intVX = format(int(self.v[int(opcode[1], 16)], 16), '03') # padded with zeroes if <100 or <10
+            self.memory[int(self.i, 16)] = intVX[0]
+            self.memory[int(self.i, 16)+1] = intVX[1]
+            self.memory[int(self.i, 16)+2] = intVX[2]
+
+        # FX55- Stores V0 to VX (including VX) in memory starting at address I.
+        elif opcodeFirstByte[0] == "F" and opcodeLastByte == "55":
+            x = int(opcode[1], 16)
+            for vIterator in range(0, x+1):
+                self.memory[int(self.i, 16)+vIterator] = self.v[vIterator]
+
+        # FX65- Fills V0 to VX (including VX) with values from memory starting at address I.
+        elif opcodeFirstByte[0] == "F" and opcodeLastByte == "65":
+            x = int(opcode[1], 16)
+            for vIterator in range(0, x+1):
+                self.v[vIterator] = self.memory[int(self.i, 16)+vIterator]
 
         self.pc += pcIncrementBy
 
