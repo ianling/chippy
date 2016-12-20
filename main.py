e936fb6ad34ea3f7112d14613404daa9ad1b533c
Ian <ebol4anthr4x@gmail.com>
2016-12-19 16:58:25 -0800

    Specify ROM on command line, add sys.exit
    Specify full path to ROM file on command line, e.g. `python main.py /home/ianl/roms/PONG`
    
    Changed quit()'s to sys.exit()

--- main.py
+++ main.py
@@ -5,32 +5,42 @@ main
 """
 from chip8 import Chip8
 import pygame
+from sys import argv, exit
+from os.path import isfile
 
 def updateScreen():
     pass
 
 def main():
-    #initialize graphics, audio
-    #see https://github.com/metulburr/pong/blob/master/data/control.py
-    #graphics = 64x32
+    # get rom path
+    rom = argv[1]
+    print rom
+    if not isfile(rom):
+        exit()
+
+    # initialize graphics
+    # see https://github.com/metulburr/pong/blob/master/data/control.py
+    # graphics = 64x32
     pygame.init()
-#    display = pygame.display.set_mode((512, 256))
     pygame.display.set_caption('chippy')
     clock = pygame.time.Clock()
+    chip8 = Chip8(pygame.display.set_mode((512, 256)), rom)
 
-#    chip8 = Chip8('M:/BACKUPS/Coding/Python/roms/PONG')
-    chip8 = Chip8(pygame.display.set_mode((512, 256)), 'C:/Users/Ian Ling/Documents/bk/chip8/roms/PONG2')
-
+    # main loop
     while True:
         # if we're updating the screen, make sure the screen only updates at 60FPS max
         if chip8.getDrawFlag():
             clock.tick(60)
             pygame.display.update()
+            # check if the player has tried to close the window
+            event = pygame.event.poll()
+            if event.type == pygame.QUIT:
+                exit()
             pygame.event.clear() # we don't use pygame events
             chip8.setDrawFlag(False)
             chip8.emulateCycle()
-        else:  # if we're not updating the screen, run other opcodes as quickly as possible
+        else:
+            # still run other opcodes, even if we aren't drawing anything
             chip8.emulateCycle()
-        #set key press flags...
 
 main()
